ec557a4c007d599cc7aa892dfceb292168da3401
eliorb <eliorb@codeoasis.com>
2014-08-17 11:58:05 +0300

    feat(maxlengthBlock); prevent user to enter too much data

--- app/scripts/directives/maxLengthBlock.js
+++ app/scripts/directives/maxLengthBlock.js
@@ -2,7 +2,19 @@ angular.module('wix.pushServer')
   .directive('maxLengthBlock', ['maxChars', function (maxChars) {
     'use strict';
     return {
-
-    }
+      restrict: 'A',
+      require: 'ngModel',
+      scope:true,
+      link: function (scope, element, attrs, ctrl) {
+        element.on('keypress', function (e) {
+          var value = ctrl.$viewValue;
+          if (value.length >= maxChars) {
+            e.preventDefault();
+            e.stopPropagation();
+            return;
+          }
+        });
+      }
+    };
   }]
 );
