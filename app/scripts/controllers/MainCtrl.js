0bb822a0bbde21fcdd65fe01c1959d761e3714d6
Nait Adar <nait@codeoasis.com>
2014-08-17 15:46:53 +0300

    style(main):fix stuff

--- app/scripts/controllers/MainCtrl.js
+++ app/scripts/controllers/MainCtrl.js
@@ -1,6 +1,12 @@
 angular.module('wix.pushServer')
-  .controller('MainController', function () {
-    this.isShownForm = false;
-  })
+  .controller('MainController', ['$scope', function ($scope) {
+    $scope.isShownForm = false;
+
+    $scope.$watch('isShownForm', function(newVal) {
+    	if (newVal && Wix) {
+    		Wix.resizeWindow(450, 400);
+    	}
+    });
+  }])
 ;
 
