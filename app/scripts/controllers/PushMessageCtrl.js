004bdc5a8030e2ece42ca5b0aebc44d57727b41d
eliorb <eliorb@codeoasis.com>
2014-08-20 18:13:38 +0300

    style(push): styling for dashboard

--- app/scripts/controllers/PushMessageCtrl.js
+++ app/scripts/controllers/PushMessageCtrl.js
@@ -5,40 +5,48 @@ angular.module('wix.pushServer')
     '$http',
     '$timeout',
     function (maxChars, serverUrl, $http, $timeout) {
-    'use strict';
-    this.notification = '';
-    this.maxChars = maxChars;
-
-    this.sendingState = {
-      isSending: false,
-      message: ''
-    };
-
-    this.postMessage = function () {
+      'use strict';
       var pushCtrl = this;
 
-      pushCtrl.sendingState.isSending = true;
-      pushCtrl.sendingState.message = 'Sending...';
-      // do ajax here
-      $http.post(serverUrl, {notification: pushCtrl.notification})
-        .then(function (response) {
-          pushCtrl.sendingState.message = 'Notification sent!';
-        })
-        .catch(function (response) {
-          pushCtrl.sendingState.message = response;
-        })
-        .finally(function () {
-          $timeout(function(){
-            pushCtrl.sendingState.isSending = false;
-            pushCtrl.sendingState.message = '';
-          }, 1500);
-
-        })
-      ;
-
       pushCtrl.notification = '';
-    };
+      pushCtrl.maxChars = maxChars;
 
-  }])
+      pushCtrl.sendingState = {
+        isSending: false,
+        message: ''
+      };
+
+      pushCtrl.submitOnEnter = function (e) {
+        if (e.which === 13) {
+          e.preventDefault();
+          pushCtrl.postMessage();
+        }
+      }
+
+      pushCtrl.postMessage = function () {
+
+        pushCtrl.sendingState.isSending = true;
+        pushCtrl.sendingState.message = 'Sending...';
+        // do ajax here
+        $http.post(serverUrl, {notification: pushCtrl.notification})
+          .then(function (response) {
+            pushCtrl.sendingState.message = 'Notification sent!';
+          })
+          .catch(function (response) {
+            pushCtrl.sendingState.message = 'Error sending notification!';
+          })
+          .finally(function () {
+            $timeout(function () {
+              pushCtrl.sendingState.isSending = false;
+              pushCtrl.sendingState.message = '';
+            }, 1500);
+
+          })
+        ;
+
+        pushCtrl.notification = '';
+      };
+
+    }])
 ;
 
